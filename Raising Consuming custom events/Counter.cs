4f87e9fa95d551b0b5b26b60e801a005e42b2175
lester88a <lester88a@gmail.com>
2015-09-23 09:45:50 -0400

    basic

---
+++ Raising Consuming custom events/Counter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Raising_Consuming_custom_events
+{
+    class Counter
+    {
+        private int threshold;
+        private int total;
+        public Counter(int passedThreshold)
+        {
+            this.threshold = passedThreshold;
+        }
+        public void Add(int x)
+        {
+            total += x;
+            if (total >=threshold)
+            {
+                OnThresholdReach(EventArgs.Empty);
+            }
+        }
+        protected virtual void OnThresholdReach(EventArgs e)
+        {
+            EventHandler hander = ThresholdReached;
+            if (hander != null)
+            {
+                hander(this, e);
+            }
+        }
+        public event EventHandler ThresholdReached;
+    }
+}
---
+++ Raising Consuming custom events/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Raising& Consuming custom events
+ * 
+ */
+
+namespace Raising_Consuming_custom_events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //example 1: event that dones not have tata
+            Counter c = new Counter(new Random().Next(10));
+            c.ThresholdReached += c_ThresholdReached;
+            Console.WriteLine("Press the key 'a' to increase");
+            while (Console.ReadKey(true).KeyChar=='q')
+            {
+                Console.WriteLine("adding one");
+                c.Add(1);
+            }
+        }
+        static void c_ThresholdReached(object sender, EventArgs e)
+        {
+            Console.WriteLine("The theshold was reached");
+            //Environment.Exit(0);
+        }
+    }
+}
